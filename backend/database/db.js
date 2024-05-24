2f9c0d95addcbcf7e0431124f1cc030850afeebd
lx-dark <164078325+lx-dark@users.noreply.github.com>
2024-05-23 23:33:51 -0300

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql";
 export const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "willdev123",
+    password: "password123",
     database: "people"
 });
 
